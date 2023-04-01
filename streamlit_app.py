4b1d115938a21180cad00ea98a8bd57797a8a353
breakingByte <129024007+breakingByte@users.noreply.github.com>
2023-04-01 17:27:23 -0600

    Update streamlit_app.py
    Fix icons and add avocado toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🌱 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🌽 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🍞 Avocado toast')
 
  
    
