7be8b9a10a78924b1a07e1508685e322d6ea2311
breakingByte <129024007+breakingByte@users.noreply.github.com>
2023-04-01 17:21:40 -0600

    Update streamlit_app.py
    add icons to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('U0001F963','Omega 3 & Blueberry Oatmeal')
+streamlit.text('U0001F957','Kale, Spinach & Rocket Smoothie')
+streamlit.text('U0001F414','Hard-Boiled Free-Range Egg')
+
+ 
