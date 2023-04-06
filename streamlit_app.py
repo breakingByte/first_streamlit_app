8053cfb18a6634e418ecb7d53197c6e952835a83
breakingByte <129024007+breakingByte@users.noreply.github.com>
2023-04-06 11:21:55 -0600

    Update streamlit_app.py
    comment the json display

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.header('🍌🍓 Fruityvice Fruit Advice 🍇🍍')
 # New section section to display fruityvice API response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
