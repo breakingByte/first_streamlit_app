92d103d7817257ef3bd0cb6884beb1509d3f2c42
breakingByte <129024007+breakingByte@users.noreply.github.com>
2023-04-06 18:48:10 -0600

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,6 +51,7 @@ streamlit.header("The fruit load list contains: ")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+         streamlit.dataframe(my_curr.fetchall())
          return my_curr.fetchall()
     
 #Add a button to load the fruit    
