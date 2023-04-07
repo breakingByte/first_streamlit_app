33713bcfa4e164a37f7cac2687c280041e55ad8e
breakingByte <129024007+breakingByte@users.noreply.github.com>
2023-04-06 19:20:48 -0600

    Update streamlit_app.py
    adding insert with variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,6 +67,10 @@ streamlit.stop()
    
 #Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+if streamlit.button('Add a fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur =my_cnx.cursor() 
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('"+ add_my_fruit +"')")
+   
 streamlit.write('Thank for adding ', add_my_fruit)
 #this will not work correctly
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit')")
